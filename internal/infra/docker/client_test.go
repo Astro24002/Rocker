5c0148a87b746152a508643a300e63f5a952c197
libk <astro24002@163.com>
2026-05-07 09:20:09 +0000

    test: add failing docker runtime source contract

--- internal/infra/docker/client_test.go
+++ internal/infra/docker/client_test.go
@@ -1,5 +1,19 @@
 package docker
 
-import "Rocker/internal/runtime"
+import (
+	"context"
+	"io"
+	"testing"
+
+	"Rocker/internal/runtime"
+)
+
+type logsContract interface {
+	ContainerLogs(context.Context, string, int) (io.ReadCloser, error)
+}
 
 var _ runtime.RuntimeSource = (*Client)(nil)
+
+func TestClientMatchesLogsContract(t *testing.T) {
+	var _ logsContract = (*Client)(nil)
+}
